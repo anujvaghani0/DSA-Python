ec76483fc3df16f8a11c531620d53aae3df4fb6a
anujvaghani0 <anujvaghani0@gmail.com>
2023-06-26 01:35:02 +0530

    added

---
+++ Arrays/ArmstrongNumber.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def armstrongNumber(self, n: int) -> bool:
+        temp=n
+        if n < 0:
+            return False
+        digits = []
+        while n > 0:
+            digits.append(n % 10)
+            n //= 10
+        k = len(digits)
+        return sum([digit ** k for digit in digits]) == temp
+
+
+solution = Solution()
+n=int(input("Enter a number: "))
+print(solution.armstrongNumber(int(n)))
